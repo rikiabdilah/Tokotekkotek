83aff34605a2d49d6d0c9b3735cc5818983753ed
anzila26 <sitianzilanur@gmail.com>
2023-05-23 10:38:20 +0700

    update splash,di,network

--- app/src/main/java/com/example/tokotekkotek/network/RetrofitClient.kt
+++ app/src/main/java/com/example/tokotekkotek/network/RetrofitClient.kt
@@ -1,4 +1,41 @@
 package com.example.tokotekkotek.network
 
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object RetrofitClient {
+    private const val  BASE_URL ="https://646b1d797d3c1cae4ce33622.mockapi.io/ "
+
+    private  val logging : HttpLoggingInterceptor
+        get(){
+            val httpLoggingInterceptor = HttpLoggingInterceptor()
+            return httpLoggingInterceptor.apply {
+                httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+            }
+        }
+
+    private val clint = OkHttpClient.Builder().addInterceptor(logging).build()
+
+    @Singleton
+    @Provides
+    fun provideRetrofit() : Retrofit =
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(clint)
+            .build()
+
+    @Singleton
+    @Provides
+    fun provideNewsApi(retrofit: Retrofit): RestfulApi =
+        retrofit.create(RestfulApi::class.java)
 }
