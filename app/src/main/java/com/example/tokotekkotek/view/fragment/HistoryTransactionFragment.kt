811027c9117de830a479c72d5041d06eb6c92216
muhammadhilmisatrio <hilmisatrio16@gmail.com>
2023-05-24 23:49:19 +0700

    add class history transaction fragment and layout

---
+++ app/src/main/java/com/example/tokotekkotek/view/fragment/HistoryTransactionFragment.kt
@@ -0,0 +1,20 @@
+package com.example.tokotekkotek.view.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.tokotekkotek.R
+
+class HistoryTransactionFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_history_transaction, container, false)
+    }
+
+}
